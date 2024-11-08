db846713cd650c88d6971d81699047183cedabe1
Jina W <70620580+hkl690@users.noreply.github.com>
2024-11-07 21:00:11 -0600

    practice

--- CodePathGroups/Unit8_Session1.py
+++ CodePathGroups/Unit8_Session1.py
@@ -80,22 +80,127 @@ ivy2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
 print(right_vine(ivy1))
 print(right_vine(ivy2))
 
-def count_leaves(root):
-    if root is None:
-        return 0
+from collections import deque 
 
-    # if the node is a leaf, return 1
-    if root.left is None and root.right is None:
-        return 1
+# Tree Node class
+class TreeNode:
+  def __init__(self, value, key=None, left=None, right=None):
+      self.key = key
+      self.val = value
+      self.left = left
+      self.right = right
 
-    left_leaves = count_leaves(root.left)
-    right_leaves = count_leaves(root.right)
+def build_tree(values):
+  if not values:
+      return None
 
-    return left_leaves + left_leaves
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
 
-oak1 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")), TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
 
-oak2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
 
-print(count_leaves(oak1))
-print(count_leaves(oak2))
+  return root
+
+from collections import deque 
+
+# Tree Node class
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
+
+class TreeNode():
+     def __init__(self, value, left=None, right=None):
+         self.val = value
+         self.left = left
+         self.right = right
+         
+def count_odd_splits(root):
+    count=0
+    stack=[]
+    
+    #cur=root
+    stack.append(root)
+    while stack:
+        cur=stack.pop()
+        if cur:
+            
+            if cur.val % 2==1:
+                count+=1
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+    return count
+
+# Using build_tree() function included at top of page
+values = [2, 3, 5, 6, 7, None, 12]
+monstera = build_tree(values)
+
+print(count_odd_splits(monstera))
+print(count_odd_splits(None))
+
+def find_flower(inventory, name):
+    while inventory:
+        if inventory.val==name:
+            return True
+        if inventory.val > name:
+            inventory=inventory.left
+        else:
+            inventory=inventory.right
+    return False
+
+# using build_tree() function at top of page
+values = ["Rose", "Lilac", "Tulip", "Daisy", "Lily", None, "Violet"]
+garden = build_tree(values)
+
+print(find_flower(garden, "Lilac"))  
+print(find_flower(garden, "Sunflower")) 
+
+def add_plant(collection, name):
+    if collection is None:
+        return 
+
+# Using build_tree() function at the top of page
+values = ["Money Tree", "Fiddle Leaf Fig", "Snake Plant"]
+collection = build_tree(values)
+
+# Using print_tree() function at the top of page
+print_tree(add_plant(collection, "Aloe"))
